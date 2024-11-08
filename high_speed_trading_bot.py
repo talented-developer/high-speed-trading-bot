a641deb06ca4f1acebd30da9a5f931d4c181b380
Anatolii Chupryna <anat.fulldev@gmail.com>
2024-11-08 10:21:58 -0600

    send message to admin after starting of bot by users

--- high_speed_trading_bot.py
+++ high_speed_trading_bot.py
@@ -3,14 +3,19 @@ from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+# Load Telegram bot token from environment variables
 load_dotenv()
-
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+admin_user_id = "8096817817"  # Admin's user ID
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user_id = update.effective_user.id
-    # await update.message.reply_text('Hello! I am high-speed-trading bot.')
+    # Send a greeting message to the user
     await update.message.reply_text(f"Hello! I am high-speed-trading bot. \n Your Telegram user Id is {user_id}.")
+
+    # Send the user's ID to the admin
+    await context.bot.send_message(chat_id=admin_user_id, text=f"New user started the bot: {user_id}")
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text('Help command!')
